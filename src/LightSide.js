5b9c90ba760a5e0d3d628c5efbfcdbac6598b4a6
powellmad <madsnpowell@gmail.com>
2021-03-17 11:30:45 -0500

    login screen renders with no errors

--- src/LightSide.js
+++ src/LightSide.js
@@ -1,13 +1,34 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
+import { Home } from './components/Home'
 import { ApplicationViews } from "./components/ApplicationViews"
-// import { Login } from "./auth/Login"
-// import { Register } from "./auth/Register"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { userStorageKey } from "./auth/authSettings"
 
 export const LightSide = () => (
     <>
         <Route path="/">
             <ApplicationViews />
         </Route>
+        <Route render={() => {
+        if (sessionStorage.getItem(userStorageKey)) {
+          return (
+            <>
+              <Home />
+            </>
+          )
+        } else {
+          return <Redirect to="/login" />;
+        }
+    }} />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+
     </>
 )
