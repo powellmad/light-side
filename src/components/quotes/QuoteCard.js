593132f03048340242367c2e1af0b5d1a98be2d1
powellmad <madsnpowell@gmail.com>
2021-03-18 13:23:11 -0500

    ?

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export const QuoteCard = ({ quote }) => (
     <section className="quote">
-        <div className="quote_text">{quote.mastertext}</div>
+        <div className="quote_text">{quote?.mastertext}</div>
     </section>
 )
