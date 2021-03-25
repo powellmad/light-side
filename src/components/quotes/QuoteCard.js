ac5c3a0afaf0a5a59cf5b14ff80cf047c73f0983
powellmad <madsnpowell@gmail.com>
2021-03-25 10:43:59 -0500

    yodatext renders when yoda is the users master

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -1,12 +1,19 @@
-import React from "react"
-
+import React, { useState, useContext, useEffect } from "react"
+import { UserContext } from "../users/UserProvider"
 
 export const QuoteCard = ({ quote }) => {
     const currentUser = parseInt(sessionStorage.getItem("app_user_id"))
+    const {getUserById } = useContext(UserContext)
+    const [userObject, setUserObject] = useState({})
+
+    useEffect(() => { 
+        getUserById(currentUser)
+            .then(setUserObject)
+    }, [])
 
     return (
         <section className="quote">
-            <div className="quote_text">"{currentUser.jediId === 1 ? quote?.yodatext : quote?.jeditext}"</div>
+            <div className="quote_text">"{userObject.jediId === 1 ? quote?.yodatext : quote?.jeditext}"</div>
         </section>
     )
 }
